5e23b2b34a6ce2976cad66f2dcd3b9cd56d91c05
Pythonesa <pythonesamar@gmail.com>
2024-07-17 18:54:22 -0300

    :sparkles:(products): add product list view and template

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductFormView
+from .views import ProductFormView, ProductsView
 
 urlpatterns = [
     path("add/", ProductFormView.as_view(), name="add_product"),
+    path("", ProductsView.as_view(), name="all_products"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 
 from products.forms import ProductForm
+from products.models import Product
 
 
 class ProductFormView(FormView):
     template_name = "products/add_product.html"
     form_class = ProductForm
-    success_url = reverse_lazy("add_product")
+    success_url = reverse_lazy("all_products")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+class ProductsView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+    context_object_name = "products"
+    template_name = "products/all_products.html"
