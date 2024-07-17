42dbec052903f1769717a01473ce40580e75aba1
Pythonesa <pythonesamar@gmail.com>
2024-07-17 06:39:43 -0300

    :sparkles: (Product): add ProductForm

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from products.models import Product
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Name")
+    description = forms.CharField(max_length=500, label="Description")
+    price = forms.DecimalField(max_digits=10, decimal_places=2, label="Price")
+    available = forms.BooleanField(initial=True, required=False, label="Available")
+    photo = forms.ImageField(required=False, label="Photo")
+
+    def save(self):
+        Product.objects.create(
+            name=self.cleaned_data["name"],
+            description=self.cleaned_data["description"],
+            price=self.cleaned_data["price"],
+            available=self.cleaned_data["available"],
+            photo=self.cleaned_data["photo"],
+        )
