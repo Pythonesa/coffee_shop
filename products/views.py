94fd6240d27bf1bd7c6ab2d2bf1099f0fa6ad40b
Pythonesa <pythonesamar@gmail.com>
2024-07-17 07:25:17 -0300

    :sparkles:(Products): add Add Product view & template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+from products.forms import ProductForm
+
+
+class ProductFormView(FormView):
+    template_name = "products/add_product.html"
+    form_class = ProductForm
+    success_url = reverse_lazy("add_product")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
