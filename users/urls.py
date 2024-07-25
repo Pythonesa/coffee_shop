49e5acdbf67a3810f492411715a865fcbac23c65
Pythonesa <pythonesamar@gmail.com>
2024-07-24 21:03:21 -0300

    :sparkles: feat(users): add sign up

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
+from users.views import RegisterView
 
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
